ae4340f47cc32b97f1cefded3a0cac6500345561
Zhen Chen <chenzhen42@qq.com>
2015-10-22 20:10:09 +1300

    Add static methods

--- Hash.java
+++ Hash.java
@@ -3,20 +3,24 @@ import java.security.NoSuchAlgorithmException;
 
 public class Hash implements Cipher {
 	private String hashType;
-	
+
 	public Hash(String hashType) {
 		this.hashType = hashType;
 	}
-	
+
 	public String getHashType() {
 		return hashType;
 	}
-	
+
 	public void setHashType(String hashType) {
 		this.hashType = hashType;
 	}
 
 	public String encode(String txt) {
+		return encode(txt, hashType);
+	}
+
+	public static String encode(String txt, String hashType) {
 		try {
 			MessageDigest md = MessageDigest.getInstance(hashType);
 			byte[] array = md.digest(txt.getBytes());
@@ -29,6 +33,14 @@ public class Hash implements Cipher {
 			// error action
 		}
 		return null;
+	}
+
+	public static String md5(String txt) {
+		return encode(txt, "MD5");
+	}
+
+	public static String sha1(String txt) {
+		return encode(txt, "SHA1");
 	}
 
 }
