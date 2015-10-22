f9eb600a0e69058465c949661c641781d4c6de89
Zhen Chen <chenzhen42@qq.com>
2015-10-22 20:36:24 +1300

    Initial commit

---
+++ Caesar.java
@@ -0,0 +1,59 @@
+/**
+ * This class can encode and decode a string using Caesar cipher based on a key;
+ * 
+ * @author Zhen Chen
+ *
+ */
+
+public class Caesar implements Cipher {
+	private int key;
+
+	public static void main(String[] args) {
+		Caesar c = new Caesar(3);
+		System.out.println(c.encode("Go the Blacks!"));
+		System.out.println(encode("Go the Blacks!", 13));
+	}
+
+	public Caesar() {
+		key = 0;
+	}
+
+	public Caesar(int key) {
+		this.key = key;
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public String encode(String txt) {
+		return encode(txt, key);
+	}
+
+	public static String encode(String txt, int key) {
+		char[] buffer = txt.toCharArray();
+		char letter;
+		int code;
+		int base;
+
+		for (int i = 0; i < buffer.length; i++) {
+			letter = buffer[i];
+			code = (int) letter;
+
+			if (letter >= 'a' && letter <= 'z') {
+				base = (int) 'a';
+				buffer[i] = (char) ((code + key - base) % 26 + base);
+			} else if (letter >= 'A' && letter <= 'Z') {
+				base = (int) 'A';
+				buffer[i] = (char) ((code + key - base) % 26 + base);
+			}
+		}
+
+		return String.valueOf(buffer);
+	}
+
+}
